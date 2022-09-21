86ce9f5798c4574d2040559f6a1ec731c7dd21ca
nhatduong01 <nhatduong30001@gmail.com>
2022-09-21 09:51:35 +0700

    set up redux and addToBasket reducer

--- App.js
+++ App.js
@@ -5,18 +5,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <stack.Navigator>
-          <stack.Screen name="Home" component={HomeScreen} />
-          <stack.Screen name="Restaurant" component={RestaurantScreen} />
-        </stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <stack.Navigator>
+            <stack.Screen name="Home" component={HomeScreen} />
+            <stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
---
+++ features/basketSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  // Actions that allow us to modify the global store
+  reducers: {
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      state.value -= 1;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+export const selectBasketItems = (state) => state.basket.items;
+
+export default basketSlice.reducer;
